d17ea7f4aeb98917a9e2a8d73baab911a39cd92a
Daniel Antonio Ceballos Flores <danielc@linqstream.com>
2021-04-14 11:52:57 -0400

    create game controller

---
+++ controllers/gameController.js
@@ -0,0 +1,21 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: async function (req, res) {
+    const result = await db.game.find();
+    res.json(result);
+  },
+  create: async function (req, res) {
+    const game = req.body;
+    const result = await db.user.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { games: [game] } }
+    );
+    res.json(result);
+  },
+  findOne: async function (req, res) {
+    console.log("request.body", req.params.id);
+    const result = await db.game.findOne({ _id: req.params.id });
+    res.json(result);
+  },
+};
