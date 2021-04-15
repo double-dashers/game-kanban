0b7fe1a89ee2064aa35ff093d08e5cb6553c5f96
Daniel Antonio Ceballos Flores <danielceballos94@gmail.com>
2021-04-15 19:52:43 -0400

    style basic layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -4,8 +4,8 @@ const Layout = (props) => {
   const { children } = props;
   return (
     <div>
-      <div>Header goes here</div>
-      <div>Children container {children}</div>
+      <div style={{height:'8vh'}}>Header goes here</div>
+      <div style={{height: '100vh'}}>Children container {children}</div>
     </div>
   );
 };
