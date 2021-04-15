7d3fc5fb5f9fffdf5ee5571da41f00437d3bb48c
LHBO19 <44046448+LHBO19@users.noreply.github.com>
2021-04-15 12:56:19 -0400

    loginController created (#1)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
-const gameController = require("../controllers/gameController")
+const gameController = require("../controllers/gameController");
+const loginController = require("../controllers/loginController");
 
 router.route("/").get(userController.findAll).post(userController.create);
 router.route("/:id").get(userController.findOne);
 router.route("/game/:id").post(gameController.create);
+router.route("/login").post(loginController.findOne);
 
 module.exports = router;
