a04b1c6ef77a7b9fedd21ff3ef5d13d8f64a4d9e
Daniel Antonio Ceballos Flores <danielceballos94@gmail.com>
2021-04-16 11:24:50 -0400

    remove material, add bootstrap

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,5 +1,3 @@
-import { Grid } from "@material-ui/core";
-import Container from '@material-ui/core/Container';
 import React from "react";
 import Navbar from "./Navbar";
 
@@ -8,17 +6,9 @@ const Layout = (props) => {
   return (
     <div>
       <Navbar />
-      <Container>
-        <div style={{ flexGrow: 1 }}>
-          <Grid
-            style={{ height: "100vh", border: "solid red" }}
-            container
-            spacing={3}
-          >
-            <Grid item xs={12}>{children}</Grid>
-          </Grid>
-        </div>
-      </Container>
+      <div>
+        Container {children}
+      </div>
     </div>
   );
 };
