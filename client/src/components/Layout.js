2d9c62924b03e9ace13408c1f6037c3856a60006
Daniel Antonio Ceballos Flores <danielceballos94@gmail.com>
2021-04-15 20:33:15 -0400

    add children props to layout with grid

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,24 @@
+import { Grid } from "@material-ui/core";
+import Container from '@material-ui/core/Container';
 import React from "react";
+import Navbar from "./Navbar";
 
 const Layout = (props) => {
   const { children } = props;
   return (
     <div>
-      <div style={{height:'8vh'}}>Header goes here</div>
-      <div style={{height: '100vh'}}>Children container {children}</div>
+      <Navbar />
+      <Container>
+        <div style={{ flexGrow: 1 }}>
+          <Grid
+            style={{ height: "100vh", border: "solid red" }}
+            container
+            spacing={3}
+          >
+            <Grid item xs={12}>{children}</Grid>
+          </Grid>
+        </div>
+      </Container>
     </div>
   );
 };
